22d01e2f56cc3f98e2bffcbe9cd05f593d94b495
Steve <stevetasticsteve@gmail.com>
2019-10-11 19:04:21 +1000

    Added a test for editing the participants form

--- CE/Tests/test_edit_CE_view.py
+++ CE/Tests/test_edit_CE_view.py
@@ -1,6 +1,7 @@
 import os
 import unittest
 import shutil
+import datetime
 from django.urls import reverse
 from django.core.files.uploadedfile import SimpleUploadedFile
 from CE import models
@@ -12,6 +13,7 @@ class TestEditPage(CETestBaseClass):
         self.assertEqual(models.CultureEvent.objects.get(pk=1).title, self.test_data['title'])
         self.assertEqual(len(models.CultureEvent.objects.all()), 2)
         self.assertEqual(len(models.TextModel.objects.all()), 2)
+        self.assertEqual(len(models.ParticipationModel.objects.all()), 1)
 
     def test_edit_page_GET_response(self):
         response = self.client.get(reverse('CE:edit', args='1'))
@@ -196,3 +198,22 @@ class TestEditPage(CETestBaseClass):
 
         finally:
             self.cleanup_test_files(1)
+
+    def test_can_edit_first_participants_form(self):
+        post_data = self.standard_post
+        post_data['participants-0-team_participants'] = 'Changed'
+        post_data['participants-0-national_participants'] = 'Changed'
+        post_data['participants-0-date'] = '2019-10-11'
+        post_data['participants-TOTAL_FORMS'] = 2
+        # import pprint
+        # pprint.pprint(post_data)
+        response = self.client.post(reverse('CE:edit', args='1'), data=post_data, follow=True)
+
+        self.assertRedirects(response, '/CE/1')
+        self.assertEqual(len(models.ParticipationModel.objects.all()), 1)
+        self.assertEqual(models.ParticipationModel.objects.get(pk=1).national_participants, 'Changed',
+                         'Participation 1 not updated on POST')
+        self.assertEqual(models.ParticipationModel.objects.get(pk=1).team_participants, 'Changed',
+                         'Participation 1 not updated on POST')
+        self.assertEqual(models.ParticipationModel.objects.get(pk=1).date, datetime.date(2019, 10, 11),
+                         'Participation 1 not updated on POST')

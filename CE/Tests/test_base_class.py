eede90c63ae659f30e7e1f80a622dfc8fc83416e
Steve <stevetasticsteve@gmail.com>
2019-10-11 16:52:34 +1000

    Created my own test class to share helper methods and test data between pages

---
+++ CE/Tests/test_base_class.py
@@ -0,0 +1,114 @@
+import os
+from django.test import TestCase
+from django.contrib.auth.models import User
+from CE import models
+
+
+class CETestBaseClass(TestCase):
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        credentials = User(username='Tester')
+        credentials.set_password('secure_password')
+        credentials.save()
+
+    def setUp(self):
+        self.test_data = {
+            'username': 'Tester',
+            'title': 'Example CE1',
+            'description_plain_text': 'A culture event happened',
+            'differences': 'Last time it was different',
+            'interpretation': 'It probably has meaning',
+            'national_participants': 'Ulumo',
+            'team_participants': 'Steve',
+            'date': '2019-02-20',
+            'phonetic_text': 'foᵘnɛtɪks',
+            'orthographic_text': 'orthographic',
+            'valid_for_DA': False,
+            'tags': 'taggie',
+            'question': 'Does this test work?',
+            'answer': 'Yes, it does!'
+        }
+        self.standard_post = {'text-TOTAL_FORMS': 2,
+                              'text-INITIAL_FORMS': 2,
+                              'question-TOTAL_FORMS': 1,
+                              'question-INITIAL_FORMS': 1,
+                              'participants-TOTAL_FORMS': 1,
+                              'participants-INITIAL_FORMS': 1,
+                              'title': self.test_data['title'],
+                              'description_plain_text': self.test_data['description_plain_text'],
+                              'tags': self.test_data['tags'],
+                              'differences': self.test_data['differences'],
+                              'interpretation': self.test_data['interpretation'],
+                              'text-0-ce': 1,
+                              'text-0-id': 1,
+                              'text-0-phonetic_text': self.test_data['phonetic_text'],
+                              'text-0-orthographic_text': self.test_data['orthographic_text'],
+                              'text-0-valid_for_DA': self.test_data['valid_for_DA'],
+                              'text-1-ce': 1,
+                              'text-1-id': 2,
+                              'text-1-phonetic_text': self.test_data['phonetic_text'],
+                              'text-1-orthographic_text': self.test_data['orthographic_text'],
+                              'text-1-valid_for_DA': self.test_data['valid_for_DA'],
+                              'participation-0-ce': 1,
+                              'participation-0-id': 1,
+                              'participation-0-team_participants': self.test_data['team_participants'],
+                              'participation-0-national_participants': self.test_data['national_participants'],
+                              'participation-0-date': self.test_data['date'],
+                              'question-0-ce': 1,
+                              'question-0-id': 1,
+                              'question-0-question': self.test_data['question'],
+                              'question-0-answer': self.test_data['answer']
+                              }
+
+        self.client.login(username='Tester', password='secure_password')
+        ce = models.CultureEvent(title=self.test_data['title'],
+                                 description_plain_text=self.test_data['description_plain_text'],
+                                 differences=self.test_data['differences'],
+                                 interpretation=self.test_data['interpretation'], )
+        ce.save()
+        ce2 = models.CultureEvent(title='CE2')
+        ce2.save()
+        ce.tags.add(self.test_data['tags'])
+        part = models.ParticipationModel(national_participants=self.test_data['national_participants'],
+                                         team_participants=self.test_data['team_participants'],
+                                         date=self.test_data['date'],
+                                         ce=ce)
+        part.save()
+        text = models.TextModel(ce=ce,
+                                phonetic_text=self.test_data['phonetic_text'],
+                                orthographic_text=self.test_data['orthographic_text'],
+                                valid_for_DA=self.test_data['valid_for_DA'])
+        text.save()
+        text = models.TextModel(ce=ce,
+                                phonetic_text='phonetic_text2',
+                                orthographic_text=self.test_data['orthographic_text'],
+                                valid_for_DA=self.test_data['valid_for_DA'])
+        text.save()
+        q = models.QuestionModel(ce=ce,
+                                 question=self.test_data['question'],
+                                 answer=self.test_data['answer'])
+        q.save()
+
+    def cleanup_test_files(self, ce):
+        ce = str(ce)
+        test_folder_audio = os.path.join(os.getcwd(), 'uploads/CultureEventFiles/' + ce + '/audio/')
+        test_folder_images = os.path.join(os.getcwd(), 'uploads/CultureEventFiles/' + ce + '/images/')
+        folders = [test_folder_audio, test_folder_images]
+        test_audio = ['test_audio1.mp3', 'test_audio2.mp3']
+        test_images = ['test_pic1.jpg', 'test_pic2.jpg']
+        for audio in test_audio:
+            try:
+                os.remove(test_folder_audio + audio)
+            except FileNotFoundError:
+                pass
+        for image in test_images:
+            try:
+                os.remove(test_folder_images + image)
+            except FileNotFoundError:
+                pass
+        for folder in folders:
+            try:
+                os.removedirs(folder)
+            except OSError:
+                pass
--- CE/Tests/test_edit_CE_view.py
+++ CE/Tests/test_edit_CE_view.py
@@ -1,115 +1,13 @@
-from django.test import TestCase
-from django.urls import reverse
-from django.contrib.auth.models import User
-from django.core.files.uploadedfile import SimpleUploadedFile
-
 import os
 import unittest
 import shutil
+from django.urls import reverse
+from django.core.files.uploadedfile import SimpleUploadedFile
 from CE import models
+from CE.Tests.test_base_class import CETestBaseClass
 
 
-class TestEditPage(TestCase):
-    @classmethod
-    def setUpClass(cls):
-        super().setUpClass()
-        credentials = User(username='Tester')
-        credentials.set_password('secure_password')
-        credentials.save()
-
-    def setUp(self):
-        self.test_data = {
-            'username': 'Tester',
-            'title': 'Example CE1',
-            'description_plain_text': 'A culture event happened',
-            'differences': 'Last time it was different',
-            'interpretation': 'It probably has meaning',
-            'national_participants': 'Ulumo',
-            'team_participants': 'Steve',
-            'date': '2019-02-20',
-            'phonetic_text': 'foᵘnɛtɪks',
-            'orthographic_text': 'orthographic',
-            'valid_for_DA': False,
-            'tags': 'taggie',
-            'question': 'Does this test work?',
-            'answer': 'Yes, it does!'
-        }
-        self.standard_post = {'text-TOTAL_FORMS': 2,
-                              'text-INITIAL_FORMS': 2,
-                              'question-TOTAL_FORMS': 1,
-                              'question-INITIAL_FORMS': 1,
-                              'participants-TOTAL_FORMS': 1,
-                              'participants-INITIAL_FORMS': 1,
-                              'title': self.test_data['title'],
-                              'description_plain_text': self.test_data['description_plain_text'],
-                              'tags': self.test_data['tags'],
-                              'differences': self.test_data['differences'],
-                              'interpretation': self.test_data['interpretation'],
-                              'text-0-ce': 1,
-                              'text-0-id': 1,
-                              'text-0-phonetic_text': self.test_data['phonetic_text'],
-                              'text-0-orthographic_text': self.test_data['orthographic_text'],
-                              'text-0-valid_for_DA': self.test_data['valid_for_DA'],
-                              'text-1-ce': 1,
-                              'text-1-id': 2,
-                              'text-1-phonetic_text': self.test_data['phonetic_text'],
-                              'text-1-orthographic_text': self.test_data['orthographic_text'],
-                              'text-1-valid_for_DA': self.test_data['valid_for_DA'],
-                              'participation-0-ce': 1,
-                              'participation-0-id': 1,
-                              'participation-0-team_participants': self.test_data['team_participants'],
-                              'participation-0-national_participants': self.test_data['national_participants'],
-                              'participation-0-date': self.test_data['date'],
-                              'question-0-ce': 1,
-                              'question-0-id': 1,
-                              'question-0-question': self.test_data['question'],
-                              'question-0-answer': self.test_data['answer']
-                              }
-
-        self.client.login(username='Tester', password='secure_password')
-        ce = models.CultureEvent(title=self.test_data['title'],
-                                 description_plain_text=self.test_data['description_plain_text'],
-                                 differences=self.test_data['differences'],
-                                 interpretation=self.test_data['interpretation'], )
-        ce.save()
-        ce2 = models.CultureEvent(title='CE2')
-        ce2.save()
-        ce.tags.add(self.test_data['tags'])
-        part = models.ParticipationModel(national_participants=self.test_data['national_participants'],
-                                         team_participants=self.test_data['team_participants'],
-                                         date=self.test_data['date'],
-                                         ce=ce)
-        part.save()
-        text = models.TextModel(ce=ce,
-                                phonetic_text=self.test_data['phonetic_text'],
-                                orthographic_text=self.test_data['orthographic_text'],
-                                valid_for_DA=self.test_data['valid_for_DA'])
-        text.save()
-        text = models.TextModel(ce=ce,
-                                phonetic_text='phonetic_text2',
-                                orthographic_text=self.test_data['orthographic_text'],
-                                valid_for_DA=self.test_data['valid_for_DA'])
-        text.save()
-        q = models.QuestionModel(ce=ce,
-                                 question=self.test_data['question'],
-                                 answer=self.test_data['answer'])
-        q.save()
-
-    def cleanup_test_files(self):
-        test_folder = os.path.join(os.getcwd(), 'uploads/CultureEventFiles/1/audio')
-        try:
-            os.remove('uploads/CultureEventFiles/1/audio/test_audio1.mp3')
-        except FileNotFoundError:
-            pass
-        try:
-            os.remove('uploads/CultureEventFiles/1/audio/test_audio2.mp3')
-        except FileNotFoundError:
-            pass
-        try:
-            os.removedirs(test_folder)
-        except OSError:
-            pass
-
+class TestEditPage(CETestBaseClass):
     def test_setup(self):
         self.assertEqual(models.CultureEvent.objects.get(pk=1).title, self.test_data['title'])
         self.assertEqual(len(models.CultureEvent.objects.all()), 2)
@@ -262,7 +160,7 @@ class TestEditPage(TestCase):
                                 '<audio controls> <source src="/uploads/CultureEventFiles/1/audio/test_audio1.mp3"></audio>')
 
         finally:
-            self.cleanup_test_files()
+            self.cleanup_test_files(1)
 
     def test_user_can_change_audio(self):
         try:
@@ -297,4 +195,4 @@ class TestEditPage(TestCase):
                                 '<audio controls> <source src="/uploads/CultureEventFiles/1/audio/test_audio2.mp3"></audio>')
 
         finally:
-            self.cleanup_test_files()
+            self.cleanup_test_files(1)

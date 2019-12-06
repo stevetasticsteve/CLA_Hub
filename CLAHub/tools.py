e903468340fd8ba01c1df6b3b5d2d36a437a7796
Steve <stevetasticsteve@gmail.com>
2019-12-06 16:01:05 +1000

    Created a check_csv function that checks the entire data set before import proceeds and can cancel it if an error is thrown

--- CLAHub/tools.py
+++ CLAHub/tools.py
@@ -14,41 +14,51 @@ def import_profiles_from_csv(file_upload):
         'village': 1,
         'name': 2
     }
-    if data_missing(data): #todo turn this into a check csv function that returns error codes. check eveything before uploading anything
-        return 'missing_data_error'
-    else:
-        villages = models.Person.villages
-        for profile in data:
-            # csv file has the value, not the key to villages. Need to loop though and find the
-            # key that matches the value
-            try:
-                village = ([item[0] for item in villages if item[1] == profile[columns['village']]][0])
-            except IndexError:
-                return 'village_spelling_error'
 
+    check = check_csv(data, columns)
+    if check == 'missing_data_error':
+        return check
+    elif check == 'village_spelling_error':
+        return check
+    elif check.startswith('missing_file_error'):
+        return check
+
+    else:
+        for profile in data:
             picture = os.path.join(BASE_DIR, 'uploads\import', profile[columns['picture']])
-            try:
-                new_profile = models.Person(
-                    village=village,
-                    name=profile[columns['name']],
-                    # need to insert CLAHubs install dir before the filename
-                    picture=picture,
-                    last_modified_by='Batch importer'
-                )
-                new_profile.save()
-            # return an error with the missing file name
-            except FileNotFoundError:
-                return 'missing_file_error%s' % (profile[columns['picture']],)
+            villages = models.Person.villages
+            village = ([item[0] for item in villages if item[1] == profile[columns['village']]][0])
+            new_profile = models.Person(
+                village=village,
+                name=profile[columns['name']],
+                # need to insert CLAHubs install dir before the filename
+                picture=picture,
+                last_modified_by='Batch importer'
+            )
+            new_profile.save()
+
 
             # todo clean up imports folder
             # todo write instructions into template
             # todo add other if conditions: if encoding error
-            # todo currently loop proceeds untill a record fails. better to all fail
         return len(data)
 
 
-def data_missing(csv_data):
+def check_csv(csv_data, columns):
     for profile in csv_data:
         for column in csv_data:
             if '' in column:
-                return True
+                return 'missing_data_error'
+        # csv file has the value, not the key to villages. Need to loop though and find the
+        # key that matches the value
+        try:
+            villages = models.Person.villages
+            village = ([item[0] for item in villages if item[1] == profile[columns['village']]][0])
+        except IndexError:
+            return 'village_spelling_error'
+
+        pic_exists = os.path.exists(os.path.join(BASE_DIR, 'uploads\import', profile[columns['picture']]))
+        if not pic_exists:
+            return 'missing_file_error%s' % (profile[columns['picture']],)
+
+    return 'ok'

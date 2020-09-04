4f7924c87416ec5591bee0dbfbf3ca8eb89e542b
steve <stevetasticsteve@gmail.com>
2020-09-04 20:12:18 +1000

    Added crontab comment

--- people/management/commands/update_profiles.py
+++ people/management/commands/update_profiles.py
@@ -5,7 +5,8 @@ from people.models import Person
 
 class Command(BaseCommand):
     help = '''If a profile\'s name is changed that change won't be reflected in the family field that refers to that 
-    record. The solution is to schedule CLAHub to trigger the save function for all profiles periodically. '''
+    record. The solution is to schedule CLAHub to trigger the save function for all profiles periodically.
+     Trigger via crontab using source venv/bin/activate && python manage.pu update_profiles'''
 
     def handle(self, **options):
         """Updates the profiles by running the save command. This will update hyperlinks in the family field

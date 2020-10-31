5cf5728b0b4d592b5073bdd3905d4c986360a366
steve <stevetasticsteve@gmail.com>
2020-10-31 20:33:20 +1000

    Changed settings to work in our environment over git

--- CLAHub/settings/development.py
+++ CLAHub/settings/development.py
@@ -2,5 +2,4 @@ from ..base_settings import *
 
 DEBUG = True
 SECRET_KEY = 'rti_2nm*$+2udk@-kkyp67mfiryu5_#9w=ei)@9t%jge+j8y-w'
-ALLOWED_HOSTS = ['localhost', '192.168.0.151'] # Allows connecting to dev server over LAN
-# python manage.py runserver 0.0.0.0:8000
+ALLOWED_HOSTS = ['localhost', '192.168.0.100']

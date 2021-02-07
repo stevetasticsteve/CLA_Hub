6eb23d9086494cbd35b74401def612ba966075d8
steve <stevetasticsteve@gmail.com>
2021-02-07 19:49:33 +0000

    Made dev server easier to work in different places

--- CLAHub/settings/development.py
+++ CLAHub/settings/development.py
@@ -2,4 +2,4 @@ from ..base_settings import *
 
 DEBUG = True
 SECRET_KEY = 'rti_2nm*$+2udk@-kkyp67mfiryu5_#9w=ei)@9t%jge+j8y-w'
-ALLOWED_HOSTS = ['localhost', '192.168.0.100']
+ALLOWED_HOSTS = ['*']

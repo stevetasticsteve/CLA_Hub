52220bbe39b01197242cfd3587d3a1e4005a6e43
steve <stevetasticsteve@gmail.com>
2021-02-14 20:22:01 +0000

    proper use of docker environment variables

--- deployment_tools/install_CLAHub.sh
+++ deployment_tools/install_CLAHub.sh
@@ -8,17 +8,18 @@ if ! sudo sh get-docker.sh; then
   echo "Docker installation failed, try to do it manually." && exit 1
 fi
 if [[ $(uname -m) == x86_64 ]]; then
-  sudo docker run -d --restart unless-stopped --name clahub -p 8000:8000 -v clahub:/code/data stevetasticsteve/clahub:latest
+  sudo docker run -d --restart unless-stopped --name clahub -p 8000:8000 \
+  -v clahub:/code/data -e DJANGO_SUPERUSER_PASSWORD=clahub stevetasticsteve/clahub:latest
 elif [[ $(uname -m) == arm32v7 ]]; then
-  sudo docker run -d --restart unless-stopped --name clahub -p 8000:8000 -v clahub:/code/data stevetasticsteve/clahub:arm
+  sudo docker run -d --restart unless-stopped --name clahub -p 8000:8000 \
+  -v clahub:/code/data -e DJANGO_SUPERUSER_PASSWORD=clahub stevetasticsteve/clahub:arm
 else
   echo "Container not available for this architecture" && exit 1
 fi
 sudo docker container exec clahub python manage.py migrate
-export DJANGO_SUPERUSER_PASSWORD="CLAHub"
 sudo docker container exec clahub python manage.py createsuperuser --noinput --username admin --email email@email.com
 echo
 echo "---"
 echo "CLAHub was installed. Access it in your browser: <localhost:8000> (on the server itself)."
 echo " or <server ip address>:8000 on other computers in your local network."
-echo "Login as admin, password=CLAHub and add users from the admin portal."
+echo "Login as admin, password=clahub and add users from the admin portal."

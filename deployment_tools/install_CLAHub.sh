9cfc52ec76dfeb4b0b23cf48fbf701ce59c12b7d
steve <stevetasticsteve@gmail.com>
2021-02-14 17:31:12 +0000

    Adjusted docker install script to work on x86 and arm architecture

--- deployment_tools/install_CLAHub.sh
+++ deployment_tools/install_CLAHub.sh
@@ -5,12 +5,19 @@
 
 curl -fsSL https://get.docker.com -o get-docker.sh
 sudo sh get-docker.sh
-sudo docker run -d --restart always --name CLAHub -p 8000:8000 -v clahub:/code/data stevetasticsteve/clahub
+if [[ $(uname -m) == x86_64 ]]; then
+  sudo docker run -d --restart unless-stopped --name CLAHub -p 8000:8000 -v clahub:/code/data stevetasticsteve/clahub:latest
+elif [[ $(uname -m) == arm32v7 ]]; then
+  sudo docker run -d --restart unless-stopped --name CLAHub -p 8000:8000 -v clahub:/code/data stevetasticsteve/clahub:arm
+else
+  echo "Container not available for this arhcitecture" && exit 1
+fi
 sudo docker container exec clahub python manage.py migrate
 export DJANGO_SUPERUSER_PASSWORD="CLAHub"
 export DJANGO_SUPERUSER_EMAIL="email@email.com"
 export DJANGO_SUPERUSER_USERNAME="admin"
 sudo docker container exec clahub python manage.py createsuperuser --noinput
+echo
 echo "---"
 echo "CLAHub was installed. Access it in your browser: <localhost:8000> (on the server itself)."
 echo " or <server ip address>:8000 on other computers in your local network."
